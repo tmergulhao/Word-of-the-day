f63fc1cd08cbe821cd4754bd9d350d2caa2fafa8
Tiago Mergulhão <me@tmergulhao.com>
2017-09-04 17:53:30 -0300

    Initial Commit

---
+++ Word of the day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Word of the day
+//
+//  Created by Tiago Mergulhão on 04/09/17.
+//  Copyright © 2017 Tiago Mergulhão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
